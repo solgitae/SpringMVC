a1a4a5e120456914f5a4885ec24d402d991c0e9d
dlworhd <devempt@gmail.com>
2022-06-08 09:49:46 +0900

    1

--- src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -1,2 +1,14 @@
-package hello.servlet.web.frontcontroller.v4;public interface ControllerV4 {
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     *
+     * @param paramMap
+     * @param model
+     * @return viewName
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model);
 }
