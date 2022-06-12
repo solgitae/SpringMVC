31a2291ed0f6e2dc4b3f62697de03793067b363b
solgitae <solgitae@gmail.com>
2022-06-12 18:23:13 +0900

    20220612

--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -2,7 +2,6 @@ package hello.servlet.web.frontcontroller.v5;
 
 import hello.servlet.web.frontcontroller.ModelView;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
