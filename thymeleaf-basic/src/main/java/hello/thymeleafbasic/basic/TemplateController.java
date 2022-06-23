f98e1e48ffd8a8383f3e01c46c6e0985508d59f8
solgitae <solgitae@gmail.com>
2022-06-23 16:33:53 +0900

    20220623

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
@@ -7,8 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/template")
 public class TemplateController {
+
     @GetMapping("/fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
     }
 }
