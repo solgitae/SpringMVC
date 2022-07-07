7a2614a3163d117a86ed7eb7a28520a09c2267c6
solgitae <solgitae@gmail.com>
2022-07-07 20:47:48 +0900

    20220707

--- exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
+++ exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -1,2 +1,39 @@
-package hello.exception.exhandler.advice;public class ExControllerAdvice {
+package hello.exception.exhandler.advice;
+
+import hello.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestControllerAdvice //대상을 안 정해주면 모든 컨트롤러에 적용된다.
+public class ExControllerAdvice {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandle(IllegalArgumentException e){
+        log.error("[exceptionHandle] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
+        log.error("[exceptionHandle] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandle(Exception e) {
+        log.error("[exceptionHandle] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+
+
+    }
 }
