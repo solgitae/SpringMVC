3780cc68c090cc4d791aafd68e512c10e50f4d8e
solgitae <solgitae@gmail.com>
2022-07-22 19:20:26 +0900

    20220722

---
+++ itemservice-db/src/main/java/hello/itemservice/service/ItemServiceV2.java
@@ -0,0 +1,45 @@
+package hello.itemservice.service;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemServiceV2 implements ItemService {
+
+    private final ItemRepositoryV2 itemRepositoryV2; //JPA
+    private final ItemQueryRepositoryV2 itemQueryRepositoryV2; //Querydsl
+
+    @Override
+    public Item save(Item item) {
+        return itemRepositoryV2.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemRepositoryV2.findById(id);
+    }
+
+    @Override
+    public List<Item> findItems(ItemSearchCond cond) { //조건이 붙기 때문에 복잡한 쿼리를 이용
+        return itemQueryRepositoryV2.findAll(cond);
+    }
+}
